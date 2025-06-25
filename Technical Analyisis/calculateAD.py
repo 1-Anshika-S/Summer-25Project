220f58f123f9807614d017b2ebee6662c54e940d
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-06-25 19:30:45 -0400

    start on calculate ad

--- Technical Analyisis/calculateAD.py
+++ Technical Analyisis/calculateAD.py
@@ -5,5 +5,19 @@
 def calculateAD(data):
     print(data)
 
+    adl_sum = 0
+    data_point_count = 0
 
+    for i in range(data_point_count):
+        todays_close = 0
+        todays_high = 0
+        todays_low = 0
+        todays_volume = 0
+
+        mvm = ((todays_close - todays_low) - (todays_high - todays_low)) / (todays_high - todays_low)
+        mfv = mvm * todays_volume
+
+        adl_sum += mfv
+
+    return adl_sum
 
